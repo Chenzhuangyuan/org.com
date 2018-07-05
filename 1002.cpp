74473c6c46d1e3a0adcce12f2cc2b9d36d18c2d0
CzyFirstUse <15172530259@163.com>
2018-07-05 09:56:18 +0800

    by 2018 7 5

--- 1002.cpp
+++ 1002.cpp
@@ -5,60 +5,43 @@
 #include <iostream>
 #include <string>
 #include <map>
-#include <stack>
 using namespace std;
 
 map<string, int> Map_Phone;
-stack<string> input_Phone;
 string key = "ABCDEFGHIJKLMNOPRSTUVWXY";   //编辑映射
 string value = "222333444555666777888999";
-
-inline string map_num(string str_Phone)
-{	
-	string Map_Num = "";
-	string::size_type it;
-	
-	for (int i = 0; i < str_Phone.length(); ++i)   //依次替代掉
-	{
-		if (str_Phone[i] == '-' || str_Phone[i] == 'Q' || str_Phone[i] == 'Z')
-			continue;
-		it = key.find(str_Phone[i]);
-		if (it != string::npos)
-			Map_Num += value[it];
-		else
-			Map_Num += str_Phone[i];
-	}
-	Map_Num.insert(3, 1, '-');   //标准化数据
-	return Map_Num;
-}
 
 int main()
 {
 	int N;   //目录中电话号码的总数
 	cin >> N;
 	string str_Phone;
+	string Map_Num;
+	string::size_type str_it;
+	map<string, int>::iterator it;
+
 	while (N-- > 0)
 	{
 		cin >> str_Phone;
-		input_Phone.push(str_Phone);
-	}
-	map<string, int>::iterator it;
-	while (!input_Phone.empty())
-	{
-		it = Map_Phone.find(input_Phone.top());
+		Map_Num = "";
+		for (int i = 0; i < str_Phone.length(); ++i)   //依次替代掉
+		{
+			if (str_Phone[i] == '-' || str_Phone[i] == 'Q' || str_Phone[i] == 'Z')
+				continue;
+			str_it = key.find(str_Phone[i]);
+			if (str_it != string::npos)
+				Map_Num += value[str_it];
+			else
+				Map_Num += str_Phone[i];
+		}
+		Map_Num.insert(3, 1, '-');   //标准化数据
+		it = Map_Phone.find(Map_Num);
 		if (it != Map_Phone.end())  //查找到，即已经存在
 			it->second += 1;
 		else
-		{
-			str_Phone = map_num(input_Phone.top());   //获取转化后标准的号码	
-			it = Map_Phone.find(str_Phone);
-			if (it != Map_Phone.end())  //查找到，即已经存在
-				it->second += 1;
-			else
-				Map_Phone.insert(pair<string, int>(str_Phone, 1));   //增加新的元素
-		}
-		input_Phone.pop();
+			Map_Phone.insert(pair<string, int>(Map_Num, 1));   //增加新的元素
 	}
+	
  	bool flag = false;
 	for (it = Map_Phone.begin(); it != Map_Phone.end(); ++it)
 		if (it->second > 1)
@@ -67,7 +50,7 @@ int main()
 			flag = true;
 		}
 	if (flag == false)
-		cout << "No duplicates" << endl;
+		cout << "No duplicates." << endl;
 
 	system("pause");
 	return 0;
